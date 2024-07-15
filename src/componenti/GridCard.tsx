bf86c8f7de233563e392aa3b57a78639d9afed8c
marilena91 <mlarocca91@gmail.com>
2024-07-15 16:57:10 +0200

    feat: update product grid

---
+++ src/componenti/GridCard.tsx
@@ -0,0 +1,19 @@
+import mockProducts from "../interface/productInterface";
+import ProductCard from "./ProductCard";
+//import "../css/GridCard.css"
+
+export default function GridCard() {
+    return(
+        <div className="center-grid">
+        <div className="products-grid">
+        {mockProducts.map((product)=>(
+            <ProductCard key={product.id} product={product} />
+        )
+
+        )}
+        </div>
+        </div>
+
+
+    );
+}
--- src/interface/productInterface.ts
+++ src/interface/productInterface.ts
@@ -1,5 +1,5 @@
 
-export interface product{
+export interface Product{
     id:string;
     name:string;
     image:string;
@@ -8,3 +8,34 @@ export interface product{
     
 }
 
+const mockProducts: Product[] = 
+[
+    //definiamo i tre prodotti
+    // mock è una mappatura dei prodotti che decido di scrivere io
+    {
+        id: "1",
+        name:"Product 1",
+        image:"https://cdn.pixabay.com/photo/2016/10/22/20/55/makeup-brushes-1761648_1280.jpg",
+        price:"9.99",
+        description:"Lorem ipsum"
+    },
+
+    {
+        id:"2",
+        name:"Product 2",
+        image:"https://cdn.pixabay.com/photo/2015/08/01/23/28/manicure-870857_1280.jpg",
+        price:"19.99",
+        description:"lorem ipsum",
+    },
+
+    {
+        id:"3",
+        name:"Product 3",
+        image:"https://cdn.pixabay.com/photo/2015/08/01/23/28/manicure-870857_1280.jpg",
+        price:"29.99",
+        description:"lorem ipsum",
+
+    },
+];
+
+export default mockProducts;
