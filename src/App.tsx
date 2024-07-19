b88487662e970034bfaee4162d77b7ddc089964f
oshiokiyo <larosamad@gmail.com>
2024-07-19 11:45:48 +0200

    authcontext per simulare login

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,36 @@ import { BrowserRouter, useLocation } from "react-router-dom";
 import "./App.css";
 import MainRoutes from "./routes/mainRoutes";
 import { CartProvider } from "./context/CartContext";
+import { AuthProvider } from "./context/AuthContext";
 import { Header } from "./componenti/Header";
 import { HeaderCheckout } from "./componenti/HeaderCheckout";
-import { Footer } from "./componenti/Footer"
+import { Footer } from "./componenti/Footer";
 
 function App() {
   return (
     <div className="App">
-      <CartProvider>
-        <BrowserRouter>
-          <ConditionalHeader/>
+      <AuthProvider>
+        <CartProvider>
+          <BrowserRouter>
+            <ConditionalHeader />
             { /*BrowserRouter permette al componente MainRouter di funzionare*/}
-            <MainRoutes/>
-          <Footer />
-        </BrowserRouter>
-      </CartProvider>
+            <MainRoutes />
+            <Footer />
+          </BrowserRouter>
+        </CartProvider>
+      </AuthProvider>
     </div>
   );
 }
 
 function ConditionalHeader() {
-  const location = useLocation()
+  const location = useLocation();
 
   if (location.pathname === "/checkout") {
-    return <HeaderCheckout />
+    return <HeaderCheckout />;
   }
 
-  return <Header />
+  return <Header />;
 }
 
 export default App;
-
